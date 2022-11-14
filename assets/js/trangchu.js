9dfad769a1e6c95117d4c711d0ff8ff50ec645f6
Predator1010 <duongquocloi1010@gmail.com>
2022-11-14 11:53:53 +0700

    change 1

--- assets/js/trangchu.js
+++ assets/js/trangchu.js
@@ -1,25 +1,27 @@
 
+new WOW().init();
+$(".was-validated").submit(function (e) {
+    e.preventDefault()
+    var isDisable = true
+    sessionStorage.setItem("isDisable", isDisable)
+    location.reload()
+});
 
-    new WOW().init();
+if (sessionStorage.getItem("isDisable") == 'true') {
+    $(".my-cv").removeClass("disabled ").addClass("yellow-cl");
+    $(".my-login-out").addClass("display-none ");
+
+} else {
+    $(".my-cv").addClass("disabled ").removeClass("yellow-cl");
+    $(".my-login-out").removeClass("display-none ");
+}
 
 
-    $(".was-validated").submit(function (e) {
-        e.preventDefault()
-       var isDisable = true
-        sessionStorage.setItem("isDisable", isDisable)
-        location.reload()
-    });
-
-    if (sessionStorage.getItem("isDisable")=='true') {
-        console.log(sessionStorage.getItem("isDisable"));
-        $(".my-cv").removeClass("disabled");
-        $(".my-login-out").addClass("display-none");
-console.log('xoa');
-    } else {
-        console.log('them lại');
-        $(".my-cv").addClass("disabled");
-        $(".my-login-out").removeClass("display-none");        
-        
+$(".form-search").submit(function (e) {
+    e.preventDefault();
+    var searchValue = {
+        province: $(".province").val(),
+        job: $(".job").val()
     }
-
-
+    console.log(searchValue);
+});
