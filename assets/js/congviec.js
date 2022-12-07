92719badb4e5b2a9164427ee72c4be00de7e15b7
Nqviet0904 <111555381+Nqviet0904@users.noreply.github.com>
2022-12-07 11:52:25 +0700

    set value input and js link

--- assets/js/congviec.js
+++ assets/js/congviec.js
@@ -1,205 +1,4 @@
-var LoadPostDateSort = {
-  status: "success",
-  total: 6,
-  lstPostTime: [
-    {
-      ngaydangId: 1,
-      tenngaydang: "Mới nhất",
-      sogio: 1,
-    },
-    {
-      ngaydangId: 2,
-      tenngaydang: "1 Ngày trước",
-      sogio: 24,
-    },
-    {
-      ngaydangId: 3,
-      tenngaydang: "7 Ngày trước",
-      sogio: 168,
-    },
-    {
-      ngaydangId: 4,
-      tenngaydang: "14 Ngày trước",
-      sogio: 336,
-    },
-    {
-      ngaydangId: 5,
-      tenngaydang: "30 Ngày trước",
-      sogio: 720,
-    },
-    {
-      ngaydangId: 6,
-      tenngaydang: "Tất cả",
-      sogio: 0,
-    },
-  ],
-};
-var LoadJobTimeSort = {
-  status: "success",
-  total: 6,
-  lstJobTimeType: [
-    {
-      loaicvId: 1,
-      tenloai: "Toàn thời gian",
-    },
-    {
-      loaicvId: 2,
-      tenloai: "Bán thời gian",
-    },
-    {
-      loaicvId: 3,
-      tenloai: "Thực tập",
-    },
-    {
-      loaicvId: 4,
-      tenloai: "Tình nguyện",
-    },
-    {
-      loaicvId: 5,
-      tenloai: "Tự do",
-    },
-    {
-      loaicvId: 6,
-      tenloai: "Khác",
-    },
-  ],
-};
-var LoadJobGroupSort = {
-  status: "success",
-  total: 17,
-  lstJobGroup: [
-    {
-      nhomnganhId: 1,
-      tennhomnganh: "Dịch vụ Tài chính",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 2,
-      tennhomnganh: "Truyền thông",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 3,
-      tennhomnganh: "Hàng tiêu dùng & bán lẻ",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 4,
-      tennhomnganh: "Khách sạn & du lịch",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 5,
-      tennhomnganh: "Kỹ thuật - công nghệ",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 6,
-      tennhomnganh: "Xây dựng",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 7,
-      tennhomnganh: "Vận tải",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 8,
-      tennhomnganh: "Dịch vụ",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 9,
-      tennhomnganh: "Giao dịch khách hàng",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 10,
-      tennhomnganh: "Bộ phận hỗ trợ",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 11,
-      tennhomnganh: "Kỹ thuật",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 12,
-      tennhomnganh: "Y tế",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 13,
-      tennhomnganh: "Sản xuất",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 14,
-      tennhomnganh: "Hỗ trợ sản xuất",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 16,
-      tennhomnganh: "Nhân sự",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 17,
-      tennhomnganh: "Kinh doanh",
-      iconnhom: null,
-    },
-    {
-      nhomnganhId: 18,
-      tennhomnganh: "Phiên dịch ",
-      iconnhom: null,
-    },
-  ],
-};
-var LoadJobExperienceSort = {
-  status: "success",
-  total: 7,
-  lstExperience: [
-    {
-      kinhNghiemId: 1,
-      tenKn: "Sinh viên mới ra trường",
-    },
-    {
-      kinhNghiemId: 2,
-      tenKn: "Dưới 1 năm",
-    },
-    {
-      kinhNghiemId: 3,
-      tenKn: "Từ 1 tới < 2 năm",
-    },
-    {
-      kinhNghiemId: 4,
-      tenKn: "Từ 2 tới < 3 năm",
-    },
-    {
-      kinhNghiemId: 5,
-      tenKn: "Từ 3 tới < 5 năm",
-    },
-    {
-      kinhNghiemId: 6,
-      tenKn: "Trên 5 năm",
-    },
-    {
-      kinhNghiemId: 7,
-      tenKn: "Sinh viên",
-    },
-  ],
-};
-var List = [
-  {
-    id:1,
-    img: "./assets/images/Gobrangding.jpg",
-    company:"Công ty cổ phần Global Online Brading",
-    adress: "TP. Hồ Chí Minh",
-    posttime:"3 ngày trước",
-    jobtype:"Thực tập",
-    tagname :"IT - Phần mềm, Công nghệ thông tin, Marketing"
-  }
-]
+
 //
 // Tìm phần tử cha của Element
 function getParent(element, selector) {
@@ -250,10 +49,8 @@ $(document).on("click", value, function () {
   }
 });
 }
-handleInput("input[name='typejob']");
 handleInput("input[name='postdate']");
-handleInput("input[name='groupjob']");
-handleInput("input[name='exp']");
+
 
 
 // lắng nghe sự kiện click
@@ -270,12 +67,92 @@ document.addEventListener("click", function (e) {
 });
 var buttonJob = document.getElementById("btn_filter_job");
 buttonJob.addEventListener("click", function () {
-  var Html = "";
   let inputNodelist = document.querySelectorAll("input");
   let inputChecked = Array.from(inputNodelist).filter(function (input) {
     if (input.checked == true) {
       return input;
     }
   });
+  if(inputChecked){
+  switch(Number(inputChecked[0].value)){
+    case 0:{
+      window.location.href = "./congviec_page1.html"
+      break;
+    }
+    case 1:{
+      window.location.href = "./dichvutaichinh.html";
+      break;
+    }
+    case 2:{
+      window.location.href = "./truyenthong.html";
+      break;
+    }
+    case 3:{
+      window.location.href = "./hangtieudungvabanle.html"
+      break;
+    }
+    case 4:{
+      window.location.href = "./khachsanvadulich.html"
+      break;
+    }
+    case 5:{
+      window.location.href = "./kythuat_congnghe.html"
+      break;
+    }
+    case 6:{
+      window.location.href = "./xaydung.html"
+      break;
+    }
+    case 7:{
+      window.location.href = "./vantai.html"
+      break;
+    }
+    case 8:{
+      window.location.href = "./dichvu.html"
+      break;
+    }
+    case 9:{
+      window.location.href = "./giaodichkhachhang.html"
+      break;
+    }
+    case 10:{
+      window.location.href = "./bophanhotro.html"
+      break;
+    }
+    case 11:{
+      window.location.href = "./kythuat.html"
+      break;
+    }
+    case 12:{
+      window.location.href = "./yte.html"
+      break;
+    }
+    case 13:{
+      window.location.href = "./sanxuat.html"
+      break;
+    }
+    case 14:{
+      window.location.href = "./hotrosanxuat.html"
+      break;
+    }
+    case 15:{
+      window.location.href = "./nhansu.html"
+      break;
+    }
+    case 16:{
+      window.location.href = "./kinhdoanh.html"
+      break;
+    }
+    case 17:{
+      window.location.href = "./phiendich.html"
+      break;
+    }
+   
+  }
+}
+else{
+  window.location.href = "./congviec_page1.html"
+}
 });
+
 
